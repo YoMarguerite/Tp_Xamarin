7eaca946d3378e6be027741f4d01d482f3fa4cdf
Unknown <yoann.marguerite@orange.fr>
2019-03-22 16:12:58 +0100

    Init
    Premier commit

---
+++ TpXamarin/TpXamarin/TpXamarin/Model/Annonce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TpXamarin.Model
+{
+    public class Annonce
+    {
+        public int Id { get; set; }
+
+        public string Titre { get; set; }
+
+        public string Description { get; set; }
+
+        public double Prix { get; set; }
+
+        public string Contact { get; set; }
+
+        public string Categorie { get; set; }
+
+        public string Date { get; set; }
+    }
+}
---
+++ TpXamarin/TpXamarin/TpXamarin/Model/Utilisateur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TpXamarin.Model
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Login { get; set; }
+
+        public string Mdp{ get; set; }
+    }
+}
